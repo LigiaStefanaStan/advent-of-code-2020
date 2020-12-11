e193a91d712ecd005020bb3a5c62fd8ce7b16f4f
LigiaStefanaStan <l.stan@elsevier.com>
2020-12-11 11:12:20 +0000

    Day 1 - Part 2

--- src/main/scala/adventofcode2020/Day1.scala
+++ src/main/scala/adventofcode2020/Day1.scala
@@ -10,13 +10,16 @@ object Day1 {
   def main(args: Array[String]): Unit = {
 
     val reportEntries = readExpenseReport(inputDataPath)
+    println("Part 1 ---------------------------------")
     findEntryMatches(reportEntries, entriesSum)
-    //Part 1 - puzzle answer is 567171
+    println("Part 2 ---------------------------------")
+    findMatchesForThreeEntries(reportEntries, entriesSum)
 
   }
 
   def readExpenseReport(path: String): Seq[Int] = Source.fromResource(path).getLines().toSeq.map(_.toInt)
 
+  //Part 1
   def findEntryMatches(entries: Seq[Int], entriesSum: Int): Seq[Int] = {
 
     entries.flatMap { x =>
@@ -28,6 +31,23 @@ object Day1 {
           }
         }
       }.collect { case i: Int => i }.toList.distinct
+
+  }
+
+  //Part 2
+  def findMatchesForThreeEntries(entries: Seq[Int], entriesSum: Int): Seq[Int] = {
+
+    entries.flatMap { x =>
+      entries.flatMap { y =>
+        entries.map { z =>
+          if (x + y + z == entriesSum) {
+            val multiplicationValue = x * y * z
+            println(s"x = $x y = $y z = $z  x * y * z = $multiplicationValue")
+            multiplicationValue
+          }
+        }
+      }
+    }.collect { case i: Int => i }.toList.distinct
 
   }
 
--- src/test/scala/adventofcode2020/Day1Spec.scala
+++ src/test/scala/adventofcode2020/Day1Spec.scala
@@ -6,14 +6,17 @@ import org.scalatest._
 class Day1Spec extends FunSuite with Matchers {
 
   val testPath = "test-data/test-day-1.txt"
-  val entriesSum = 10
 
   test("read entries from file") {
     readExpenseReport(testPath) shouldBe Seq(1, 2, 3, 4, 5)
   }
 
-  test("find the entries that match the sum and multiply them") {
-    findEntryMatches(readExpenseReport(testPath), entriesSum) shouldBe Seq(25)
+  test("find the product of two entries that match the sum") {
+    findEntryMatches(readExpenseReport(testPath), 10) shouldBe Seq(25)
+  }
+
+  test("find the product of three entries that match the sum") {
+    findMatchesForThreeEntries(readExpenseReport(testPath), 8) shouldBe Seq(10, 12, 16, 18)
   }
 
 }
