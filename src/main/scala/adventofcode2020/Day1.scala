4a317a55543ee13781f968d36c106a1c882c67ac
LigiaStefanaStan <l.stan@elsevier.com>
2020-12-11 22:21:28 +0000

    Replace flatMaps and maps with for-yields

--- src/main/scala/adventofcode2020/Day1.scala
+++ src/main/scala/adventofcode2020/Day1.scala
@@ -8,47 +8,38 @@ object Day1 {
   val entriesSum = 2020
 
   def main(args: Array[String]): Unit = {
-
     val reportEntries = readExpenseReport(inputDataPath)
     println("Part 1 ---------------------------------")
     findEntryMatches(reportEntries, entriesSum)
     println("Part 2 ---------------------------------")
     findMatchesForThreeEntries(reportEntries, entriesSum)
-
   }
 
   def readExpenseReport(path: String): Seq[Int] = Source.fromResource(path).getLines().toSeq.map(_.toInt)
 
   //Part 1
   def findEntryMatches(entries: Seq[Int], entriesSum: Int): Seq[Int] = {
-
-    entries.flatMap { x =>
-      entries.map { y =>
-          if (x + y == entriesSum) {
-            val multiplicationValue = x * y
-            println(s"x = $x y = $y x * y = $multiplicationValue")
-            multiplicationValue
-          }
-        }
-      }.collect { case i: Int => i }.toList.distinct
-
+    for {
+      x <- entries
+      y <- entries
+      if x + y == entriesSum
+    } yield {
+      println(s"x = $x y = $y  x * y = " + x * y)
+      x * y
+    }
   }
 
   //Part 2
   def findMatchesForThreeEntries(entries: Seq[Int], entriesSum: Int): Seq[Int] = {
-
-    entries.flatMap { x =>
-      entries.flatMap { y =>
-        entries.map { z =>
-          if (x + y + z == entriesSum) {
-            val multiplicationValue = x * y * z
-            println(s"x = $x y = $y z = $z  x * y * z = $multiplicationValue")
-            multiplicationValue
-          }
-        }
-      }
-    }.collect { case i: Int => i }.toList.distinct
-
+    (for {
+      x <- entries
+      y <- entries
+      z <- entries
+      if x + y + z == entriesSum
+    } yield {
+      println(s"x = $x y = $y  z = $z  x * y * z = " + x * y * z)
+      x * y * z
+    }).distinct
   }
 
 }
